b961bac2f7d7cf847a26bae4b6f66c96909b53a0
Ričardas Bielskas <ricardas.bielskass1@gmail.com>
2023-10-17 11:41:43 +0300

    missing files

---
+++ Shared/ShipCoordinate.cs
@@ -0,0 +1,20 @@
+﻿namespace backend.Models.Entity.Ships;
+
+public class ShipCoordinate
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsHit { get; private set; } = false;
+    public string Icon { get; set; } = ShipCoordinateIcon.Ship;
+
+    public ShipCoordinate(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public void Hit()
+    {
+        IsHit = true;
+    }
+}
---
+++ Shared/ShipCoordinateIcon.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.Models.Entity.Ships
+{
+    public class ShipCoordinateIcon
+    {
+        public static readonly string Ship = @"icons\ship.png";
+        public static readonly string Explosion = @"icons\explosion.png";
+    }
+}
