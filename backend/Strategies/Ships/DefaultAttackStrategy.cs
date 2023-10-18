cdeaf3b0c46cd866dadab7e9e281afe180f95c1d
siuramka <marius.kamarauskas2001@gmail.com>
2023-10-18 18:11:59 +0300

    add default attack strategy

---
+++ backend/Strategies/Ships/DefaultAttackStrategy.cs
@@ -0,0 +1,26 @@
+﻿using backend.Models.Entity.Bombs;
+using backend.Models.Entity.Ships;
+
+namespace backend.Strategies.Ships
+{
+    public class DefaultAttackStrategy : IAttackStrategy
+    {
+        public List<ShipCoordinate> TargetShip(int x, int y, MissileBomb missileBomb, int gameBoardSizeX, int gameBoardSizeY)
+        {
+            List<ShipCoordinate> coordinates = new List<ShipCoordinate>();
+
+            coordinates.Add(new ShipCoordinate(x, y)); // 1x1
+
+            return coordinates;
+        }
+
+        public List<ShipCoordinate> TargetShip(int x, int y, AtomicBomb atomicBomb, int gameBoardSizeX, int gameBoardSizeY)
+        {
+            List<ShipCoordinate> coordinates = new List<ShipCoordinate>();
+
+            coordinates.Add(new ShipCoordinate(x, y)); // 1x1
+
+            return coordinates;
+        }
+    }
+}
